5af02b6ce95450e7a6d64fab57daf2cb5b39be8d
Mark Millard <mmillard@alticast.com>
2018-06-19 12:17:53 -0600

    Moving responsibility for mlLoadWorkprint to Digital Workprint library.

---
+++ DigitalWorkprint/lib/common/include/mle/DwpLoad.h
@@ -0,0 +1,62 @@
+/** @defgroup MleDWPAccess Magic Lantern Digital Workprint Library API - Base */
+
+/**
+ * @file DwpLoad.h
+ * @ingroup MleDWPAccess
+ *
+ * This file defines the loader used by the Magic Lantern Digital
+ * Workprint Library API.
+ */
+
+// COPYRIGHT_BEGIN
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2018 Wizzer Works
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//  For information concerning this header file, contact Mark S. Millard,
+//  of Wizzer Works at msm@wizzerworks.com.
+//
+//  More information concerning Wizzer Works may be found at
+//
+//      http://www.wizzerworks.com
+//
+// COPYRIGHT_END
+
+#ifndef __MLE_DWPLOAD_H_
+#define __MLE_DWPLOAD_H_
+
+
+// Include Magic Lantern header files.
+#include "mle/mlMalloc.h"
+
+// Include Magic Lantern Digital Workprint header files.
+#include "mle/DwpLoad.h"
+
+class MleDwpItem;
+
+// mlLoadWorkprint() is a utility call that loads the indicated
+//   workprint from the file system and roots the hierarchy at
+//   _mlWorkprint.  Zero is returned in the event of an error.
+extern MLE_DWP_API MleDwpItem *mlLoadWorkprint(const char *filename);
+
+
+#endif /* __MLE_DWPLOAD_H_ */
---
+++ DigitalWorkprint/lib/common/src/DWPBase/DwpLoad.cpp
@@ -0,0 +1,179 @@
+/** @defgroup MleDWPAccess Magic Lantern Digital Workprint Library API - Base */
+
+/**
+ * @file DwpLoad.cpp
+ * @ingroup MleDWPAccess
+ *
+ * This file implements the loader used by the Magic Lantern Digital
+ * Workprint Library API.
+ */
+
+// COPYRIGHT_BEGIN
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2018 Wizzer Works
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//  For information concerning this source code, contact Mark S. Millard,
+//  of Wizzer Works at msm@wizzerworks.com.
+//
+//  More information concerning Wizzer Works may be found at
+//
+//      http://www.wizzerworks.com
+//
+// COPYRIGHT_END
+
+// Include system header files.
+#include <stdio.h>
+
+// Include Magic Lantern header files.
+#include "mle/mlAssert.h"
+
+// Include Digital Workprint header files.
+#include "mle/Dwp.h"
+#include "mle/DwpItem.h"
+#include "mle/DwpDSOFile.h"
+#include "mle/DwpSet.h"
+#include "mle/DwpGroup.h"
+#include "mle/DwpScene.h"
+#include "mle/DwpMediaRef.h"
+#include "mle/DwpFinder.h"
+
+// Include ATK header files.
+//#include "mle/MlePlayer.h"
+
+//MLE_DWP_API MlePlayer *_mlPlayer = NULL;
+MLE_DWP_API MleDwpItem *_mlWorkprint =  NULL;
+
+MLE_DWP_API MleDwpItem *mlLoadWorkprint(const char *filename)
+{
+	MLE_ASSERT(filename);
+
+	// Set the flag to load DSOs when they are specified.
+	MleDwpDSOFile::setLoadOnSet(1);
+
+	// Read in the workprint.
+	_mlWorkprint = MleDwpItem::readAll(filename);
+
+	return _mlWorkprint;
+}
+
+#if 0
+static void releaseItem(MleDwpItem *item)
+{
+	if ( _mlPlayer )
+	{
+		// Find the root.
+		while ( item->getParent() )
+			item = item->getParent();
+		
+		delete item;
+	}
+}
+
+MleDwpGroup *_mlGetWorkprintGroup(const char *id)
+{
+	if ( _mlPlayer )
+		_mlWorkprint = _mlPlayer->sendGetWorkprintGroup(id);
+
+	MLE_ASSERT(_mlWorkprint);
+#if defined(WIN32)
+    MleDwpFinder groupFinder("MleDwpGroup",id,0);
+#else
+	MleDwpFinder groupFinder(MleDwpGroup::typeId,id,0);
+#endif
+	return (MleDwpGroup *)groupFinder.find(_mlWorkprint);
+}
+
+void _mlReleaseWorkprintGroup(MleDwpItem *group)
+{
+	releaseItem(group);
+}
+
+MleDwpScene *_mlGetWorkprintScene(const char *id)
+{
+	if ( _mlPlayer )
+		_mlWorkprint = _mlPlayer->sendGetWorkprintScene(id);
+
+	MLE_ASSERT(_mlWorkprint);
+#if defined(WIN32)
+	MleDwpFinder sceneFinder("MleDwpScene",id,0);
+#else
+	MleDwpFinder sceneFinder(MleDwpScene::typeId,id,0);
+#endif
+	return (MleDwpScene *)sceneFinder.find(_mlWorkprint);
+}
+
+void _mlReleaseWorkprintScene(MleDwpItem *scene)
+{
+	releaseItem(scene);
+}
+
+MleDwpMediaRef *_mlGetWorkprintMediaRef(const char *id)
+{
+	if ( _mlPlayer )
+	{
+		_mlWorkprint = _mlPlayer->sendGetWorkprintMediaRef(id);
+
+		// If we fail, just return NULL.
+		if ( _mlWorkprint == NULL )
+			return NULL;
+	}
+
+	MLE_ASSERT(_mlWorkprint);
+#if defined(WIN32)
+	MleDwpFinder mediaRefFinder("MleDwpMediaRef",id,0);
+#else
+	MleDwpFinder mediaRefFinder(MleDwpMediaRef::typeId,id,0);
+#endif
+	return (MleDwpMediaRef *)mediaRefFinder.find(_mlWorkprint);
+}
+
+void _mlReleaseWorkprintMediaRef(MleDwpMediaRef *mediaRef)
+{
+	releaseItem(mediaRef);
+}
+
+MleDwpSet *_mlGetWorkprintSet(const char *id)
+{
+	if ( _mlPlayer )
+	{
+		_mlWorkprint = _mlPlayer->sendGetWorkprintSet(id);
+
+		// If we fail, just return NULL.
+		if ( _mlWorkprint == NULL )
+			return NULL;
+	}
+
+	MLE_ASSERT(_mlWorkprint);
+#if defined(WIN32)
+	MleDwpFinder setFinder("MleDwpSet",id,0);
+#else
+	MleDwpFinder setFinder(MleDwpSet::typeId,id,0);
+#endif
+	return (MleDwpSet *)setFinder.find(_mlWorkprint);
+}
+
+void _mlReleaseWorkprintSet(MleDwpItem *set)
+{
+	releaseItem(set);
+}
+#endif /* 0 */
