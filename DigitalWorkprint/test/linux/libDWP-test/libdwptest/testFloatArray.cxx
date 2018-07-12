3aa7aabc8120dbf117cda21ef43e71d00495dbe2
Mark Millard <mmillard@alticast.com>
2018-07-12 14:27:49 -0600

    Fixed reference to new DwpLoad.h file for mlLoadWorkprint.

--- DigitalWorkprint/test/linux/libDWP-test/libdwptest/testFloatArray.cxx
+++ DigitalWorkprint/test/linux/libDWP-test/libdwptest/testFloatArray.cxx
@@ -2,7 +2,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 - 2018 Wizzer Works
+// Copyright (c) 2016-2018 Wizzer Works
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -46,6 +46,7 @@
 #include <mle/DwpProperty.h>
 #include <mle/DwpDatatype.h>
 #include <mle/DwpFloatArray.h>
+#include <mle/DwpLoad.h>
 
 class FloatArrayTestFixture: public ::testing::Test
 {
--- DigitalWorkprint/test/linux/libDWP-test/libdwptest/testMlVector3.cxx
+++ DigitalWorkprint/test/linux/libDWP-test/libdwptest/testMlVector3.cxx
@@ -2,7 +2,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 - 2018 Wizzer Works
+// Copyright (c) 2016-2018 Wizzer Works
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -45,6 +45,7 @@
 #include <mle/DwpProperty.h>
 #include <mle/DwpDatatype.h>
 #include <mle/DwpVector3.h>
+#include <mle/DwpLoad.h>
 
 class MlVector3TestFixture: public ::testing::Test
 {
